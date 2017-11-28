913b58fc44982bed3f2426110c422cf788a0ae3e
pluboj <pluboj@yahoo.com>
2017-11-28 15:19:54 -0800

    Add progressbar

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.ttk import Progressbar
+import time
+import threading
 
 
 class App:
@@ -26,10 +29,25 @@ class App:
         # File chooser button
         search_img = PhotoImage(file="images/Search-Folder-icon.png")
         search_img = search_img.subsample(5)
-        btn_search = Button(master, text="Select File")
-        btn_search.config(image=search_img, width="240", height="80", border=2, cursor='hand2')
-        btn_search.grid(row=0, column=1, sticky='e' + 'w', padx=50, pady=20)
-        btn_search.image = search_img
+        self.btn_search = Button(master, text="Select File", command=self.process_file)
+        self.btn_search.config(image=search_img, width="240", height="80", border=2, cursor='hand2')
+        self.btn_search.grid(row=0, column=1, sticky='e' + 'w', padx=50, pady=20)
+        self.btn_search.image = search_img
+
+        # Progressbar
+        self.progress = Progressbar(master, orient=HORIZONTAL, length=245, mode='indeterminate')
+
+    def process_file(self):
+        def progress():
+            self.progress.grid(row=1, column=1, sticky='e', padx=50)
+            self.progress.start()
+            time.sleep(5)
+            self.progress.stop()
+            self.progress.grid_forget()
+            self.btn_search['state'] = 'normal'
+
+        self.btn_search['state'] = 'disabled'
+        threading.Thread(target=progress).start()
 
     @staticmethod
     def turn_off():
