e57177253de7f889d615efedeff2a4e25c9adfb2
pluboj <pluboj@yahoo.com>
2017-11-28 00:32:50 -0800

    Update layout

--- Main.py
+++ Main.py
@@ -13,8 +13,16 @@ class App:
         Label(master, text="Directory:", bg="LightSalmon2", font=("Helvetica", 12))\
             .grid(row=0, column=0, sticky='e', pady=20)
         global input_dir
-        input_dir = Entry(master, width=60, bg="RosyBrown2")
-        input_dir.grid(row=0, column=1, padx=2, pady=2, sticky='we', columnspan=9)
+        # input_dir = Entry(master, width=60, bg="RosyBrown2")
+        # input_dir.grid(row=0, column=1, padx=2, pady=2, sticky='we', columnspan=9)
+        dir = Text(master, height=2, width=45, bg="RosyBrown2", state=DISABLED, relief="flat")
+        dir.grid(row=0, column=1, padx=2, pady=2, sticky='we', columnspan=9)
+        search_img = PhotoImage(file="images/search_f.png")
+        search_img = search_img.subsample(12)
+        btn_search = Button(master, text="Select File")
+        btn_search.config(image=search_img, width="40", height="40", border=0)
+        btn_search.grid(row=0, column=10, sticky='e' + 'w', padx=10, pady=2)
+        btn_search.image = search_img
 
 
 root = Tk()
