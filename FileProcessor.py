b7169583e80aa17dac62d98fc329e9a62135cac0
pluboj <pluboj@yahoo.com>
2017-11-29 17:04:45 -0800

    Update process

--- FileProcessor.py
+++ FileProcessor.py
@@ -6,6 +6,14 @@ class FileProcessor:
     def __init__(self, file):
         self.file = file
 
+    ''' 
+    Excel format:
+        column A: new file variable ID
+        column B: new file label
+        column C: old file variable ID
+        column D: old file label
+    '''
+
     def process(self):
         wb = openpyxl.load_workbook(self.file)
         sheet = wb.get_sheet_by_name('Sheet1')
@@ -15,26 +23,33 @@ class FileProcessor:
 
         for row in range(1, sheet.max_row + 1):
             current_row = str(row)
-            variable_a = sheet['A' + current_row].value
-            variable_b = sheet['C' + current_row].value
-            label_a = sheet['B' + current_row].value
-            label_b = sheet['D' + current_row].value
-            if variable_a and label_a:
-                data_new_file[row] = {'var': variable_a, 'label': label_a}
-            if variable_b and label_b:
-                data_old_file[row] = {'var': variable_b, 'label': label_b}
+            new_var = sheet['A' + current_row].value
+            old_var = sheet['C' + current_row].value
+            new_label = sheet['B' + current_row].value
+            old_label = sheet['D' + current_row].value
+            if new_var and new_label:
+                data_new_file[row] = {'var': new_var, 'label': new_label}
+            if old_var and old_label:
+                data_old_file[row] = {'var': old_var, 'label': old_label}
 
         for item in data_new_file:
             label = data_new_file[item]['label']
+            tmp_old_item = []
 
             for row in data_old_file:
                 if label == data_old_file[row]['label']:
-                    print(row)
-                    data_final[item] = {'updated': 1, 'var': data_old_file[item]['var'],
-                                        'label': data_old_file[item]['label']}
-                else:
-                    data_final[item] = {'updated': 0, 'var': data_new_file[item]['var'],
-                                        'label': data_new_file[item]['label']}
+                    if item in data_old_file:
+                        tmp_old_item.append({'updated': 1, 'var': data_old_file[row]['var'],
+                                            'label': data_old_file[row]['label']})
+
+            if len(tmp_old_item) == 1:
+                data_final[item] = tmp_old_item[0]
+            else:
+                data_final[item] = {'updated': 0, 'var': data_new_file[item]['var'],
+                                    'label': data_new_file[item]['label']}
 
         for item in data_final:
-            print(str(item) + ' :: ' + str(data_final[item]))
+            print(str(item) + ' :: '
+                  + str(data_final[item]['updated']) + ' : '
+                  + str(data_final[item]['var']) + ' : '
+                  + str(data_final[item]['label']))
